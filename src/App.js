b821975b5e97cd6e44517172c91aebab7517190c
jaducku <jaducku@kakao.com>
2021-09-15 21:11:04 +0800

    Finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import { API } from 'aws-amplify';
 
 function App() {
+  const [coins, updateCoins] = useState([]);
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+  function updateInputvalue(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
+
+  async function fetchCoins() {
+    const { limit, start } = input;
+
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
+    updateCoins(data.coins);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input onChange={e => updateInputvalue('start', e.target.value)}
+          placeholder="start" />
+        <input onChange={e => updateInputvalue('limit', e.target.value)}
+          placeholder="limit" />
+        <button onClick={fetchCoins}>Fetch</button>
+      </div>
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
     </div>
   );
 }
